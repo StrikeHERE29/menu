332cd7f2e10c69f0cf0172d3572a78541c8cce45
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-10 22:35:04 +0200

    created the object that holds the items

---
+++ menu.js
@@ -0,0 +1,50 @@
+const menu = [
+    {
+        id:1,
+        title: "Super Burger",
+        category: "Lunch",
+        price: "$14.99",
+        img: "https://images.unsplash.com/photo-1550547660-d9450f859349?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age."
+    },
+    {
+        id:2,
+        title: "Omelette",
+        category: "Breakfast",
+        price: "$4.99",
+        img: "https://plus.unsplash.com/premium_photo-1671403963864-6d46f3b62352?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Shy resolution instrument unreserved man few. She did open find pain some out. If we landlord stanhill mr whatever pleasure supplied concerns so."
+    },
+    {
+        id:3,
+        title: "Waffles",
+        category: "Breakfast",
+        price: "$3.99",
+        img: "https://images.unsplash.com/photo-1550547660-d9450f859349?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Certain but she but shyness why cottage. Gay the put instrument sir entreaties affronting. Pretended exquisite see cordially the you. Weeks quiet do vexed or whose. Motionless if no to affronting imprudence no precaution."
+    },
+    {
+        id:4,
+        title: "Steak",
+        category: "Dinner",
+        price: "$24.99",
+        img: "https://images.unsplash.com/photo-1600891964092-4316c288032e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"My indulged as disposal strongly attended. Parlors men express had private village man. Discovery moonlight recommend all one not. Indulged to answered prospect it bachelor is he bringing shutters. Pronounce forfeited mr direction oh he dashwoods ye unwilling."
+    },
+    {
+        id:5,
+        title: "Roasted Ribs",
+        category: "Dinner",
+        price: "$30.99",
+        img: "https://images.unsplash.com/photo-1544025162-d76694265947?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Ladies others the six desire age. Bred am soon park past read by lain. As excuse eldest no moment. An delight beloved up garrets am cottage private. The far attachment discovered celebrated decisively surrounded for and."
+    },
+    {
+        id:6,
+        title: "Pasta",
+        category: "Lunch",
+        price: "$10.99",
+        img: "https://images.unsplash.com/photo-1611270629569-8b357cb88da9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Feel sold off felt nay rose met you. We so entreaties cultivated astonished is. Was sister for few longer mrs sudden talent become. Done may bore quit evil old mile. If likely am of beauty tastes."
+    }
+]
